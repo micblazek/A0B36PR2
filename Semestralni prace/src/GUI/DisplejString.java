62eea122ff3c6f2b213bb78760d2e3a7da01d4eb
blazemi9 <blazemi9@fel.cvut.cz>
2013-04-03 15:52:12 +0200

    Oprava rozdělení vstupu do zlomků v1.1

--- Semestralni prace/src/GUI/DisplejString.java
+++ Semestralni prace/src/GUI/DisplejString.java
@@ -43,6 +43,8 @@ public class DisplejString {
                         k += 2;
                     }
                 }
+            }
+            if (vstup.length() != 0) {
                 if (jmenovatel) {
                     if (list.size() < k + 1) {
                         list.add(k, vstup);
@@ -50,18 +52,19 @@ public class DisplejString {
                     }
                     list.set(k, vstup);
                     list.set(k + 1, 2);
-                    if (vstup.length() != 0) {
-                        if (vstup.charAt(i) == '\t') {
-                            list.set(k, vstup);
-                            list.set(k + 1, 2);
-                            jmenovatel = false;
-                            citatel = false;
-                            vstup = vstup.substring(i + 1, vstup.length());
-                            i = 0;
-                            k += 2;
-                        }
+                    if (vstup.charAt(i) == '\t') {
+                        list.set(k, vstup.substring(0, i));
+                        list.set(k + 1, 2);
+                        jmenovatel = false;
+                        citatel = false;
+                        vstup = vstup.substring(i + 1, vstup.length());
+                        i = 0;
+                        k += 2;
                     }
+
                 }
+            }
+            if (vstup.length() != 0) {
                 if (jmenovatel == false && citatel == false) {
                     if (list.size() < k + 1) {
                         list.add(k, vstup);
